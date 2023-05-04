411ba7b0b7ffe100c5ce200bd0f6300277fbb8e5
Alex Luna <alexlunapuerto@gmail.com>
2023-05-04 19:09:53 +0100

    Feature added: getAlbumDetails

--- back/routes/albumRoute.js
+++ back/routes/albumRoute.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { getAlbumTracks } = require("../controllers/albumController");
+const { getAlbumDetails } = require("../controllers/albumController");
 
 router.get("/getAlbumTracks", getAlbumTracks);
+router.get("/getAlbumDetails", getAlbumDetails);
 
 module.exports = router;
